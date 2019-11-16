5346531c0680efb94d89b25bac2df51a98a56df9
Aleś Bułojčyk <alex73mail@gmail.com>
2019-11-16 09:35:26 +0300

    New packer

--- Android/app/src/main/java/org/alex73/android/dzietkam/catalog/Item.java
+++ Android/app/src/main/java/org/alex73/android/dzietkam/catalog/Item.java
@@ -1,7 +1,10 @@
 package org.alex73.android.dzietkam.catalog;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
 
 public class Item {
     public String id;
@@ -10,7 +13,31 @@ public class Item {
     public String description;
     public String cover;
     public FileInfo file;
-    public Map<String,String> settings;
+    public Map<String, String> settings;
     public List<Item> items; // TODO
     public transient Item parent;
+    public transient File f, coverFile;
+    public transient List<String> settingsOrder;
+
+    public long getSize() {
+        if (f.isDirectory()) {
+            long sz = 0;
+            for (File ff : FileUtils.listFiles(f, null, true)) {
+                sz += ff.length();
+            }
+            return sz;
+        } else {
+            return f.length();
+        }
+    }
+
+    public int count(String expectedType) {
+        int c = expectedType.equals(type) ? 1 : 0;
+        if (items != null) {
+            for (Item sub : items) {
+                c += sub.count(expectedType);
+            }
+        }
+        return c;
+    }
 }
